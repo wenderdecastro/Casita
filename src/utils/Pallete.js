6a6f6b33ec8fd8b114d01bd913732c58153f55f4
Wender <wendermaiac@gmail.com>
2024-05-26 18:45:39 -0300

    Init

---
+++ src/utils/Pallete.js
@@ -0,0 +1,21 @@
+export const Colors = {
+    //Primary colors
+	yellow: '#F8E32B',
+	green: '#2FC82D',
+	red: '#EE3C3C',
+	blue: '#4862EA',
+
+    //Secondary colors
+    secondary: '#FFFDDA',
+
+    //Neutral colors
+	black: '#000000',
+	gray40: '#656565',
+	gray30: '#A7A7A7',
+	gray20: '#D3D3D3',
+	gray10: '#F2F2F2',
+	white: '#FFFFFF',
+
+	transparent: 'rgba(255, 0, 0, 0.0);',
+	
+};
