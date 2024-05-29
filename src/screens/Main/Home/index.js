25a02f23d0eb977e09b58c2baacd2f112e6d634b
Guilherme Sousa Oliveira <guilhermesousa1110@gmail.com>
2024-05-29 10:29:58 -0300

    correction

--- src/screens/Main/Home/index.js
+++ src/screens/Main/Home/index.js
@@ -1,30 +1,12 @@
 import React from 'react'
 import { AppContainer } from '../../../components/AppContainers'
-import { H1 } from '../../../components/AppFonts'
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart
-} from "react-native-chart-kit";
-import { AppColors } from '../../../utils/Pallete';
-import { Dimensions } from 'react-native';
-import ProgressCardWidget from '../widgets/ProgressCardWidget';
 
 
 export default function HomeScreen() {
 
   return (
     <AppContainer>
-      <ProgressCardWidget
-      title={'Você está quase lá!'}
-      subtitle={'Tarefas semanais'}
-      total={2000}
-      actualProgress={450}
-      tagText={'3 tarefas restantes'}
-      />
+      <Text>HomeScreen</Text>
     </AppContainer>
   )
 }
