29b295ffa6418850f6975916b6325bfc7ae5d868
Luiz Henrique <luiz.henrique31415@gmail.com>
2024-06-03 09:16:11 -0300

    Fazendo pagina de Login. Commit para dar merge.

--- src/screens/Authentication/LoginPage/index.js
+++ src/screens/Authentication/LoginPage/index.js
@@ -1,10 +1,28 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { AppContainer } from '../../../components/AppContainers'
+import { AppColors } from '../../../utils/Pallete'
+import LeadingButtonWidget from '../widgets/LeadingButtonWidget'
+import Navigation from '../Navigation'
+import styled from 'styled-components'
+import { BodyMedium, TitleBlack } from '../../../components/AppFonts'
 
-export default function LoginScreen() {
+const LeadingBox = styled.View`
+position: absolute;
+top: 8%;
+left: 8%;
+`
+
+export default function LoginScreen({navigation}) {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+
+    <AppContainer backgroundColor={AppColors.background}>
+      <LeadingBox>
+        <LeadingButtonWidget navigation={navigation}/>
+
+        <TitleBlack size={32}>LOGIN</TitleBlack>
+        <BodyMedium color={AppColors.black}>Já é dos nossos? Se loga aí!</BodyMedium>
+      </LeadingBox>
+    </AppContainer>
   )
 }
