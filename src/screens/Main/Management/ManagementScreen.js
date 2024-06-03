e987cd41ecccacb41ea03abeeddce74245b8888c
Guilherme Sousa Oliveira <guilhermesousa1110@gmail.com>
2024-06-03 10:15:04 -0300

    ManagementScreen

--- src/screens/Main/Management/ManagementScreen.js
+++ src/screens/Main/Management/ManagementScreen.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { AppContainer } from '../../../components/AppContainers'
+import { AppColors } from '../../../utils/Pallete'
 
 export default function ManagementScreen() {
   return (
-    <AppContainer>
+    <AppContainer backgroundColor={AppColors.background}>
       <Text>ManagementScreen</Text>
     </AppContainer>
   )
